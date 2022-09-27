8ebc5e7de55bf7aa0e3fc2f9eee4e5e5905d5cb6
Jennifer Miller <JENNIFER.MILLER6704@STU.MTEC.EDU>
2022-09-27 13:35:20 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+$(".navItem");
+$(".navItem").css("color", "white");
+$("#nav");
+$("#nav").css("background-color", "#145");
+$("li:even").css("background-color", "#ccc");
+$("li:odd").css("background-color", "#aaa");
+$("input#myInput").val("Jennifer");
+
+
